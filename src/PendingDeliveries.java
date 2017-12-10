33ce9adeac5cdc44b93c68d87d319663c496f41e
Heng Hui Jing <Asus@192.168.0.8>
2017-12-10 13:21:27 +0800

    As a HR executive, I want to retrieve pending deliveries so that I can know which deliveries are pending.

--- src/PendingDeliveries.java
+++ src/PendingDeliveries.java
@@ -55,7 +55,7 @@ public class PendingDeliveries extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "OrderId", "Customer ID", "Order Time", "Address"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
